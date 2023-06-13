8363de5555d8f98e39fdf83c339abbc368046b3f
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-13 11:52:51 +0700

    Create Item.cs

---
+++ Library Management System/Library Management System/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    // Abstract class for library items
+    public abstract class Item
+    {
+        protected string title;
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        // Template method
+        public void PrintInfo()
+        {
+            Console.WriteLine("Title: " + title);
+            TemplateItemInfo();
+            Console.WriteLine();
+        }
+
+        // Abstract method to be implemented by subclasses
+        protected abstract void TemplateItemInfo();
+        public abstract void UpdateInfo(string newTitle, int newYearOfPublication);
+    }
+}
