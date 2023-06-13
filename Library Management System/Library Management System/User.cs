10f3fae3f28d54794a781ecffc9093239a55d214
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-13 11:53:28 +0700

    Create User.cs

---
+++ Library Management System/Library Management System/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    // Class representing a user
+    public class User
+    {
+        private string name;
+
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
