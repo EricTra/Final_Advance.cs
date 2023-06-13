6c5b7f57642d99e3e9991aa8b1c060b74a1b8493
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-13 11:52:53 +0700

    Create ItemFactory.cs

---
+++ Library Management System/Library Management System/ItemFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    // Factory class for creating items
+    public class ItemFactory
+    {
+        public Item GetItem(string itemType, string title, string authorEditor, int yearOfPublication)
+        {
+            switch (itemType.ToLower())
+            {
+                case "book":
+                    return new Book(title, authorEditor, yearOfPublication);
+                case "magazine":
+                    return new Magazine(title, authorEditor, yearOfPublication);
+                default:
+                    throw new ArgumentException("Invalid item type: " + itemType);
+            }
+        }
+    }
+}
