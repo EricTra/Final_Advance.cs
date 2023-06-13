c8f037d6972878a12c76b68685cf49ef3f0f2aa9
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-13 11:53:01 +0700

    Create Magazine.cs

---
+++ Library Management System/Library Management System/Magazine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    // Concrete class for magazines
+    public class Magazine : Item
+    {
+        private string editor;
+        private int yearOfPublication;
+
+        public Magazine(string title, string editor, int yearOfPublication)
+        {
+            this.title = title;
+            this.editor = editor;
+            this.yearOfPublication = yearOfPublication;
+        }
+
+        protected override void TemplateItemInfo()
+        {
+            Console.WriteLine("Editor: " + editor);
+            Console.WriteLine("Year of Publication: " + yearOfPublication);
+        }
+
+        public override void UpdateInfo(string newTitle, int newYearOfPublication)
+        {
+            title = newTitle;
+            yearOfPublication = newYearOfPublication;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Magazine Info:");
+            base.PrintInfo();
+        }
+    }
+
+}
